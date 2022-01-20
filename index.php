785a494ba0a75d3de92207b130cce7d11d9a0bc8
Yoga Aji Sukma <instandev@gmail.com>
2022-01-20 15:35:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 // Set your server key (Note: Server key for sandbox and production mode are different)
-$server_key = 'SB-Mid-server-eS6vBaqtSQ74OVk1bgZUbygl';
+$server_key = 'Mid-server-0ZQdQQcQ-bBVgCXtug64Is8q';
 // Set true for production, set false for sandbox
-$is_production = false;
+$is_production = true;
 
 $api_url = $is_production ? 
   'https://app.midtrans.com/snap/v1/transactions' : 
